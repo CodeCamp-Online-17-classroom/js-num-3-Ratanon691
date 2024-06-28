7d3f5348a96a34fff11a765b22823a55247f8493
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-28 18:20:08 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-// write code here
+function point(){
+let purchase = +prompt(`How much did you purchase?`)
+let points = Math.floor(purchase / 100)
+if(points > 1) {
+alert(`You get ${points} points for this purchase`)
+}else if (points ==1) {
+  alert(`You get a point for this purchase`)
+}
+  else{
+    alert(`Your purchase is too small to collect point`)
+}
+}
